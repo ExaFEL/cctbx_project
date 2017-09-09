47bd3cefae82e6bc83da330be0b2383f49e69c94
Nicholas Sauter <nksauter@lbl.gov>
2017-09-09 10:03:32 -0700

    Link against the github (live) version of Andrews-Bernstein NCDist.  Wrap both the old and live versions in boost python so results can be tested for equality.  Switch Zeldin clustering to use the live (2017) version.

--- cctbx/uctbx/determine_unit_cell/ext.cpp
+++ cctbx/uctbx/determine_unit_cell/ext.cpp
@@ -8,15 +8,25 @@ namespace af = scitbx::af;
 
 using namespace boost::python;
 
+namespace ncdist2017 {
+#include <NCDist.h>
+}
+
 namespace cctbx { namespace uctbx {
 
   double NCDist_wrapper(af::tiny<double,6> mm1,af::tiny<double,6> mm2){
     return NCDist(&mm1[0],&mm2[0]);
   }
 
+  double NCDist2017_wrapper(af::tiny<double,6> mm1,af::tiny<double,6> mm2){
+    return ncdist2017::NCDist(&mm1[0],&mm2[0]);
+  }
+
 }}
+
 
 BOOST_PYTHON_MODULE(determine_unit_cell_ext)
 {
   def ("NCDist",&cctbx::uctbx::NCDist_wrapper);
+  def ("NCDist2017",&cctbx::uctbx::NCDist2017_wrapper);
 }
