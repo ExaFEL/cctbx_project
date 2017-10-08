4aead7c1e966c1414d6b8d41a408d2b728e3ebcc
Nicholas Sauter <nksauter@lbl.gov>
2017-10-08 15:24:09 -0700

    In Rodriguez/Laio, optimize the rho step in C++

--- xfel/clustering/ext.cpp
+++ xfel/clustering/ext.cpp
@@ -5,13 +5,54 @@
 #include <boost/python/def.hpp>
 #include <scitbx/array_family/flex_types.h>
 #include <scitbx/array_family/shared.h>
+#include <scitbx/array_family/versa.h>
+#include <scitbx/array_family/accessors/c_grid.h>
 
 namespace sx_clustering {
   void foo2(){
     std::cout<<"HELLO foo2"<<std::endl;
   }
 
-  class Rodriguez_Laio_Clustering_2014 {
+  struct Rodriguez_Laio_clustering_2014 {
+
+    Rodriguez_Laio_clustering_2014( ):rho_set(false){}
+
+    Rodriguez_Laio_clustering_2014(double d_c):d_c(d_c),rho_set(false){}
+
+    Rodriguez_Laio_clustering_2014(
+      scitbx::af::flex_double distance_matrix,
+      double d_c):
+        distance_matrix( distance_matrix ),
+        d_c(d_c),
+        rho_set(false){
+        NN = distance_matrix.accessor().focus()[0];
+        SCITBX_ASSERT(NN == distance_matrix.accessor().focus()[1]);// square matrix
+    }
+
+    scitbx::af::shared<std::size_t>
+    get_rho(){
+      if (!rho_set) {
+        rho = scitbx::af::shared<std::size_t>(NN);
+        const double* Dij = distance_matrix.begin();
+        for (int i=0; i < NN; ++i){
+          for (int j=0; j < NN; ++j){
+            if (Dij[i*NN + j] < d_c) { rho[i]+=1; }
+          }
+        }
+        rho_set=true;
+      }
+      return rho;
+    }
+
+    public:
+      scitbx::af::shared<std::size_t> rho;
+
+    private:
+      scitbx::af::flex_double distance_matrix;
+      double d_c;
+      bool rho_set;
+      std::size_t NN;
+
   };
 
 }
@@ -26,14 +67,19 @@ namespace boost_python { namespace {
     typedef return_value_policy<return_by_value> rbv;
     typedef default_call_policies dcp;
 
-    class_<Rodriguez_Laio_Clustering_2014>("Rodriguez_Laio_Clustering_2014",init<>())
+    class_<Rodriguez_Laio_clustering_2014>("Rodriguez_Laio_clustering_2014",no_init)
+      .def(init<>())
+      .def(init<double >((arg_("d_c"))))
+      .def(init<scitbx::af::flex_double, double >(
+          (arg_("distance_matrix"),arg_("d_c"))))
+      .def("get_rho",&Rodriguez_Laio_clustering_2014::get_rho)
     ;
 
     def("foo2",&sx_clustering::foo2);
   }
 
 }
-}} // namespace lunus::boost_python::<anonymous>
+}} // namespace sx_clustering::boost_python::<anonymous>
 
 BOOST_PYTHON_MODULE(sx_clustering_ext)
 {
