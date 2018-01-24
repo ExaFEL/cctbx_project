2de02a223155554ab5a986c808c11f4249c5d0fd
Lee O'Riordan <loriordan@lbl.gov>
2018-01-24 15:45:31 -0800

    STRUMPACK test and scipy build run and agree

--- scitbx/examples/bevington/strumpack_solver_ext.cpp
+++ scitbx/examples/bevington/strumpack_solver_ext.cpp
@@ -38,7 +38,8 @@ typedef int intType;
 namespace sparse_solver {
   //template<typename numType, typename intType> 
   struct my_solver{
-    my_solver(intType n)//int argc, char* argv[])
+
+    /*my_solver(intType n)//int argc, char* argv[])
     {
       StrumpackSparseSolver<numType,intType> spss;
       spss.options().set_mc64job(0);
@@ -82,9 +83,67 @@ namespace sparse_solver {
       x_res.swap(x);
 
       std::cout << "# COMPONENTWISE SCALED RESIDUAL = " << A.max_scaled_residual(x_res.data(), b_res.data()) << std::endl;
+    } */
+
+    /*
+    CSRMatrix<numType,intType> af_to_csr( scitbx::af::shared<intType> A_row_offset, 
+                                          scitbx::af::shared<intType> A_col_offset, 
+                                          scitbx::af::shared<numType> A_values)
+    {
+            CSRMatrix<numType,intType> A(N, nnz);
+
+    }*/
+
+    my_solver( const int n_rows, const int n_cols,
+               const scitbx::af::shared<intType> A_row_offset, 
+               const scitbx::af::shared<intType> A_col_offset, 
+               const scitbx::af::shared<numType> A_values,
+               const scitbx::af::shared<numType> b){//int argc, char* argv[])
+      StrumpackSparseSolver<numType,intType> spss;
+      spss.options().set_mc64job(0);
+      //spss.options().set_reordering_method(ReorderingStrategy::GEOMETRIC);
+      //spss.options().set_from_command_line(argc, argv);
+
+      std::size_t nnz = A_values.size();
+      std::size_t gridSize = n_rows * n_cols;// (A_row_offset.size()-1) * (scitbx::af::max(A_col_offset)+1); //(Length of rows -1) * (the largest index+1) gives the full grid
+
+      //The number of row entries-1 will be the number of different equations, thus the size of the b-vector and x-vector
+      std::cout << "Got here 1 \n";
+      scitbx::af::shared<numType> x(3,0.0);//(A_row_offset.size()-1, numType(0.));
+      std::cout << "Got here 2 \n";
+
+      const intType* r_ptr = A_row_offset.begin();
+      const intType* c_ptr = A_col_offset.begin();
+      const numType* val_ptr = A_values.begin();
+
+      spss.set_csr_matrix(3, r_ptr, c_ptr, val_ptr, false);
+      std::cout << "Got here 3 \n";
+      SCITBX_EXAMINE (*( r_ptr ));
+      SCITBX_EXAMINE (*( c_ptr ));
+      SCITBX_EXAMINE (*( val_ptr ));
+
+      //CSRMatrix<numType,intType> AA(3, nnz);
+
+
+      //spss.reorder();//( n_rows, n_cols);//(A_row_offset.size()-1), ( scitbx::af::max(A_col_offset)+1 ) );
+      std::cout << "Got here 4 \n";
+
+      x_res.push_back(0.);x_res.push_back(0.);x_res.push_back(0.);
+      std::cout << x_res.size() << "\n";
+
+      spss.solve(b.begin(), x_res.begin());
+      std::cout << "Got here 5 \n";
+
+      // just a check, system is already solved, so solving again
+      // with the solution as initial guess should stop immediately
+      //spss.solve(b.data(), x.data(), true);
+      //x_res.swap(x);
+
+      //std::cout << "# COMPONENTWISE SCALED RESIDUAL = " << A.max_scaled_residual(x_res.data(), b_res.data()) << std::endl;
     }
-    std::vector<numType> b_res;
-    std::vector<numType> x_res;
+
+    scitbx::af::shared<numType> x_res;
+    scitbx::af::shared<numType> b_res;
   }; //end of struct sparse_solver
 
 namespace boost_python{
@@ -94,10 +153,22 @@ namespace {
   {
     typedef return_value_policy<return_by_value> rbv;
     //templated constructor for int and size_t flex arrays of id
-    class_<sparse_solver::my_solver>("sparse_solver", no_init)
-      .def(init<int>(arg("n")))
+    class_<sparse_solver::my_solver> ("sparse_solver", no_init)
+      .def( init< int, int, 
+                 scitbx::af::shared<intType>,
+                 scitbx::af::shared<intType>,
+                 scitbx::af::shared<numType>,
+                 scitbx::af::shared<numType> >
+      ((  
+        boost::python::arg("n_rows"), 
+        boost::python::arg("n_cols"), 
+        boost::python::arg("A_row_offset"), 
+        boost::python::arg("A_col_offset"), 
+        boost::python::arg("A_values"), 
+        boost::python::arg("b")
+      ))
+      )
       .add_property("x",make_getter(&sparse_solver::my_solver::x_res, rbv()))
-      .add_property("b",make_getter(&sparse_solver::my_solver::b_res, rbv()))
     ;
   }
 
@@ -106,6 +177,6 @@ namespace {
 }//sparse_solver
 BOOST_PYTHON_MODULE(scitbx_examples_strumpack_solver_ext)
 {
-   //def("sparse_solver", sparse_solver, args("n"), "My docstring");
    sparse_solver::boost_python::export_strumpack_solver();
 }
+
---
+++ scitbx/examples/bevington/test_sparse_scipy.py
@@ -0,0 +1,13 @@
+#Small-scale test script for STRUMPACK solution, compared with scipy 
+import numpy as np
+import scipy.sparse
+import scipy.sparse.linalg
+
+A_row_offset = np.array([0,3,5,8])
+A_col_offset = np.array([0,1,2,1,2,0,1,2])
+A_values = np.array([1.,2.,1.,1.,2.,3.,2.,1.])
+A = scipy.sparse.csr_matrix((A_values, A_col_offset, A_row_offset))
+b = np.array([2,0,1])
+
+x = scipy.sparse.linalg.spsolve(A, b)
+print x
