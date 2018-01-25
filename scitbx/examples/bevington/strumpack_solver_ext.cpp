596818d8657d60259fe80bde628db4c6406ec16a
Lee O'Riordan <loriordan@lbl.gov>
2018-01-25 11:08:02 -0800

    Added comments and tidied code

--- scitbx/examples/bevington/strumpack_solver_ext.cpp
+++ scitbx/examples/bevington/strumpack_solver_ext.cpp
@@ -21,7 +21,7 @@
  * Developers: Pieter Ghysels, Francois-Henry Rouet, Xiaoye S. Li.
  *             (Lawrence Berkeley National Lab, Computational Research Division).
  *
- * Modified by Lee J. O'Riordan to adapt for use with Boost.Python
+ * Modified by Lee J. O'Riordan to adapt for use with cctbx via Boost.Python 
  */
 #include <iostream>
 #include "StrumpackSparseSolver.hpp"
@@ -37,10 +37,24 @@ typedef int intType;
 
 namespace sparse_solver {
   struct my_solver{
-    my_solver( const int n_rows, const int n_cols,
-               const scitbx::af::shared<intType> A_row_offset, 
-               const scitbx::af::shared<intType> A_col_offset, 
-               const scitbx::af::shared<numType> A_values,
+    /**
+      Constructor for the STRUMPACK Ax=b sparse solver my_solver.
+      This object will be callable from python upon importing the appropriate extension module using
+      'from scitbx.examples.bevington import sparse_solver as ss'
+      Result vector x_res will be initialised by constructor to the same size as n_rows to contain solution.
+
+      @param[in]  n_rows Row count of the A matrix.
+      @param[in]  n_cols Column count of the A matrix.
+      @param[in]  A_row_offset Row offset indices for CSR sparse format matrix.
+      @param[in]  A_col_idx Column indices for CSR sparse format matrix.
+      @param[in]  A_values A matrix values in CSR sparse format matrix.
+      @param[in]  b Right hand side vector of linear system.
+      @return Description of returned value.
+    */
+    my_solver(const int n_rows, const int n_cols,
+              const scitbx::af::shared<intType> A_row_offset, 
+              const scitbx::af::shared<intType> A_col_idx, 
+              const scitbx::af::shared<numType> A_values,
               const scitbx::af::shared<numType> b) : x_res(n_rows, 0.){//int argc, char* argv[])
       StrumpackSparseSolver<numType,intType> spss;
       spss.options().set_mc64job(0);
@@ -50,35 +64,37 @@ namespace sparse_solver {
       std::size_t nnz = A_values.size();
       std::size_t gridSize = n_rows * n_cols;
 
+      //Taking raw pointer to flex data types
       const intType* r_ptr = A_row_offset.begin();
-      const intType* c_ptr = A_col_offset.begin();
+      const intType* c_ptr = A_col_idx.begin();
       const numType* val_ptr = A_values.begin();
 
+      //Set the sparse matrix values using the CSR formatted data
       spss.set_csr_matrix(n_rows, r_ptr, c_ptr, val_ptr, false);
-      spss.reorder( n_rows, n_cols);//(A_row_offset.size()-1), ( scitbx::af::max(A_col_offset)+1 ) );
+      spss.reorder( n_rows, n_cols );
+
+      //Solve Ax=b
       spss.solve(b.begin(), x_res.begin());
     }
-    scitbx::af::shared<numType> x_res; //Result vector X. Will be initialised by constructor to the same size as n_rows
+    scitbx::af::shared<numType> x_res; //Resulting x will be stored here
   }; //end of struct sparse_solver
 
 namespace boost_python{
 namespace {
-
-  void export_strumpack_solver()
-  {
+  void export_strumpack_solver() {
     typedef return_value_policy<return_by_value> rbv;
-    //templated constructor for int and size_t flex arrays of id
     class_<sparse_solver::my_solver> ("sparse_solver", no_init)
       .def( init< int, int, 
                  scitbx::af::shared<intType>,
                  scitbx::af::shared<intType>,
                  scitbx::af::shared<numType>,
-                 scitbx::af::shared<numType> >
+                 scitbx::af::shared<numType> 
+                >
       ((  
         boost::python::arg("n_rows"), 
         boost::python::arg("n_cols"), 
         boost::python::arg("A_row_offset"), 
-        boost::python::arg("A_col_offset"), 
+        boost::python::arg("A_col_idx"), 
         boost::python::arg("A_values"), 
         boost::python::arg("b")
       ))
@@ -86,7 +102,6 @@ namespace {
       .add_property("x",make_getter(&sparse_solver::my_solver::x_res, rbv()))
     ;
   }
-
 }//anon
 }//boost_python
 }//sparse_solver
