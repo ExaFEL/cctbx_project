d6336005c359e324f9d91c6ee4d39e5fe5790141
Lee O'Riordan <loriordan@lbl.gov>
2018-01-24 16:29:23 -0800

    Tidied and generalised solver

--- scitbx/examples/bevington/strumpack_solver_ext.cpp
+++ scitbx/examples/bevington/strumpack_solver_ext.cpp
@@ -36,114 +36,29 @@ typedef double numType;
 typedef int intType;
 
 namespace sparse_solver {
-  //template<typename numType, typename intType> 
   struct my_solver{
-
-    /*my_solver(intType n)//int argc, char* argv[])
-    {
-      StrumpackSparseSolver<numType,intType> spss;
-      spss.options().set_mc64job(0);
-      spss.options().set_reordering_method(ReorderingStrategy::GEOMETRIC);
-      //spss.options().set_from_command_line(argc, argv);
-
-      intType N = n * n;
-      intType nnz = 5 * N - 4 * n;
-      CSRMatrix<numType,intType> A(N, nnz);
-      intType* col_ptr = A.get_ptr();
-      intType* row_ind = A.get_ind();
-      numType* val = A.get_val();
-
-      nnz = 0;
-      col_ptr[0] = 0;
-      for (intType row=0; row<n; row++) {
-        for (intType col=0; col<n; col++) {
-          intType ind = col+n*row;
-          val[nnz] = 4.0;
-          row_ind[nnz++] = ind;
-          if (col > 0)  { val[nnz] = -1.0; row_ind[nnz++] = ind-1; } // left
-          if (col < n-1){ val[nnz] = -1.0; row_ind[nnz++] = ind+1; } // right
-          if (row > 0)  { val[nnz] = -1.0; row_ind[nnz++] = ind-n; } // up
-          if (row < n-1){ val[nnz] = -1.0; row_ind[nnz++] = ind+n; } // down
-          col_ptr[ind+1] = nnz;
-        }
-      }
-      //A.set_symmetric_sparsity();
-
-      std::vector<numType> b(N, numType(1.)), x(N, numType(0.));
-
-      spss.set_csr_matrix(N, col_ptr, row_ind, val, true);
-      spss.reorder(n, n);
-      // spss.factor();   // not really necessary, called if needed by solve
-      spss.solve(b.data(), x.data());
-
-      // just a check, system is already solved, so solving again
-      // with the solution as initial guess should stop immediately
-      //spss.solve(b.data(), x.data(), true);
-      b_res.swap(b);
-      x_res.swap(x);
-
-      std::cout << "# COMPONENTWISE SCALED RESIDUAL = " << A.max_scaled_residual(x_res.data(), b_res.data()) << std::endl;
-    } */
-
-    /*
-    CSRMatrix<numType,intType> af_to_csr( scitbx::af::shared<intType> A_row_offset, 
-                                          scitbx::af::shared<intType> A_col_offset, 
-                                          scitbx::af::shared<numType> A_values)
-    {
-            CSRMatrix<numType,intType> A(N, nnz);
-
-    }*/
-
     my_solver( const int n_rows, const int n_cols,
                const scitbx::af::shared<intType> A_row_offset, 
                const scitbx::af::shared<intType> A_col_offset, 
                const scitbx::af::shared<numType> A_values,
-               const scitbx::af::shared<numType> b){//int argc, char* argv[])
+              const scitbx::af::shared<numType> b) : x_res(n_rows, 0.){//int argc, char* argv[])
       StrumpackSparseSolver<numType,intType> spss;
       spss.options().set_mc64job(0);
       //spss.options().set_reordering_method(ReorderingStrategy::GEOMETRIC);
       //spss.options().set_from_command_line(argc, argv);
 
       std::size_t nnz = A_values.size();
-      std::size_t gridSize = n_rows * n_cols;// (A_row_offset.size()-1) * (scitbx::af::max(A_col_offset)+1); //(Length of rows -1) * (the largest index+1) gives the full grid
-
-      //The number of row entries-1 will be the number of different equations, thus the size of the b-vector and x-vector
-      std::cout << "Got here 1 \n";
-      scitbx::af::shared<numType> x(3,0.0);//(A_row_offset.size()-1, numType(0.));
-      std::cout << "Got here 2 \n";
+      std::size_t gridSize = n_rows * n_cols;
 
       const intType* r_ptr = A_row_offset.begin();
       const intType* c_ptr = A_col_offset.begin();
       const numType* val_ptr = A_values.begin();
 
-      spss.set_csr_matrix(3, r_ptr, c_ptr, val_ptr, false);
-      std::cout << "Got here 3 \n";
-      SCITBX_EXAMINE (*( r_ptr ));
-      SCITBX_EXAMINE (*( c_ptr ));
-      SCITBX_EXAMINE (*( val_ptr ));
-
-      //CSRMatrix<numType,intType> AA(3, nnz);
-
-
-      //spss.reorder();//( n_rows, n_cols);//(A_row_offset.size()-1), ( scitbx::af::max(A_col_offset)+1 ) );
-      std::cout << "Got here 4 \n";
-
-      x_res.push_back(0.);x_res.push_back(0.);x_res.push_back(0.);
-      std::cout << x_res.size() << "\n";
-
+      spss.set_csr_matrix(n_rows, r_ptr, c_ptr, val_ptr, false);
+      spss.reorder( n_rows, n_cols);//(A_row_offset.size()-1), ( scitbx::af::max(A_col_offset)+1 ) );
       spss.solve(b.begin(), x_res.begin());
-      std::cout << "Got here 5 \n";
-
-      // just a check, system is already solved, so solving again
-      // with the solution as initial guess should stop immediately
-      //spss.solve(b.data(), x.data(), true);
-      //x_res.swap(x);
-
-      //std::cout << "# COMPONENTWISE SCALED RESIDUAL = " << A.max_scaled_residual(x_res.data(), b_res.data()) << std::endl;
     }
-
-    scitbx::af::shared<numType> x_res;
-    scitbx::af::shared<numType> b_res;
+    scitbx::af::shared<numType> x_res; //Result vector X. Will be initialised by constructor to the same size as n_rows
   }; //end of struct sparse_solver
 
 namespace boost_python{
